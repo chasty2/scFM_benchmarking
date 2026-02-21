af67aa9731ccb4eab0a9be4b2f3acbcf165fad66
chasty2 <email@codyhasty.net>
2026-02-20 18:20:02 -0600

    Create training data dataclass for trrust classification

---
+++ src/scfm_utils/trrust/__init__.py
@@ -0,0 +1,15 @@
+from scfm_utils.trrust.classifier import TRRClassifierModel
+from scfm_utils.trrust.training_data import (
+    REGULATION_LABELS,
+    TRRUSTData,
+    TRRUSTRecord,
+    load_trrust_data,
+)
+
+__all__ = [
+    "REGULATION_LABELS",
+    "TRRClassifierModel",
+    "TRRUSTData",
+    "TRRUSTRecord",
+    "load_trrust_data",
+]
