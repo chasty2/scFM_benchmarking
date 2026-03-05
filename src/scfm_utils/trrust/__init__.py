a5f62ebc65f5601cc1c6953cb071556e1673d086
chasty2 <email@codyhasty.net>
2026-03-05 11:47:59 -0600

    Load binary and ternary datasets into TRRUSTData

--- src/scfm_utils/trrust/__init__.py
+++ src/scfm_utils/trrust/__init__.py
@@ -1,17 +1,29 @@
 from scfm_utils.trrust.classifier import TRRClassifierModel
 from scfm_utils.trrust.training_data import (
+    BINARY_LABEL_NAMES,
+    BINARY_LABELS,
     REGULATION_LABEL_NAMES,
     REGULATION_LABELS,
+    TERNARY_LABEL_NAMES,
+    TERNARY_LABELS,
     TRRUSTData,
     TRRUSTRecord,
+    load_binary_trrust_data,
+    load_ternary_trrust_data,
     load_trrust_data,
 )
 
 __all__ = [
+    "BINARY_LABEL_NAMES",
+    "BINARY_LABELS",
     "REGULATION_LABEL_NAMES",
     "REGULATION_LABELS",
+    "TERNARY_LABEL_NAMES",
+    "TERNARY_LABELS",
     "TRRClassifierModel",
     "TRRUSTData",
     "TRRUSTRecord",
+    "load_binary_trrust_data",
+    "load_ternary_trrust_data",
     "load_trrust_data",
 ]
