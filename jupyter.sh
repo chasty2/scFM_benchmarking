889d1ca9f22b3e58c679d6186e153b784cbd4b44
chasty2 <email@codyhasty.net>
2026-03-18 18:42:00 -0500

    Disable timeouts on jupyter.sh for long-running jobs

--- jupyter.sh
+++ jupyter.sh
@@ -1,1 +1,4 @@
-uv run --with jupyter jupyter lab --ip=0.0.0.0 --port=8888
+#!/bin/bash
+
+# Disable timeouts for long-running jobs on notebooks
+uv run --with jupyter jupyter lab --ip=0.0.0.0 --port=8888 --ServerApp.shutdown_no_activity_timeout=0 --MappingKernelManager.cull_idle_timeout=0
